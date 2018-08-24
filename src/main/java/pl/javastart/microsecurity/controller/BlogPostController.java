3a378884bc7227ea211ffe7bc92c780ffb12ac4e
Slawek Ludwiczak <slawek@javastart.pl>
2018-08-24 15:39:57 +0200

    optimize imports

--- src/main/java/pl/javastart/microsecurity/controller/BlogPostController.java
+++ src/main/java/pl/javastart/microsecurity/controller/BlogPostController.java
@@ -1,15 +1,11 @@
 package pl.javastart.microsecurity.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.javastart.microsecurity.model.BlogPost;
 import pl.javastart.microsecurity.repository.BlogPostRepository;
-
-import java.security.Principal;
 
 @Controller
 public class BlogPostController {
